d28d1ce5de1b8785ec28e6a96a4c991ecfebda74
dann toliver <dann@bentobox.net>
2011-06-18 02:50:50 -0400

    rearranged index.html; added individual start/stop buttons for sections; de-globalized variables; now storing data for each invocation seperately; seperate setInterval loops.

--- README.txt
+++ README.txt
@@ -1,4 +1,5 @@
 A library for textual manipulation.
 
-Requires jQuery.js and underscore.js.
+mistranscribe.js mutablizes text. It's extensible (you can easily add more abilities to it). It's packaged as a jQuery plugin, because I like jQuery. It requires underscore.js because the code looks prettier that way. All the good bits are just plain old javascript though, so you shouldn't have any trouble porting it to the environment of your choice.
 
+http://mistranscri.be/
--- mistranscribe.js
+++ mistranscribe.js
@@ -1,49 +1,60 @@
-(function($){  
+(function($){
+  
+  // the counter and extensions are stored for the whole page, not each individual invocation
+  var MSTRNSCRB = {
+    counter: 0,
+    extensions: []
+  };
+
   var methods = {
-    // initialize our datastore... for better or worse
-    init: function() {
-      // we set up a global repository for transformers so we can hit them all with one setInterval loop
-      // THINK: there's probably a cleaner way of doing this...
-      if(typeof(MSTRNSCRB) == 'undefined') {
-        MSTRNSCRB = {};
-        MSTRNSCRB.transformers = {};
-        MSTRNSCRB.extensions = [];
-        MSTRNSCRB.settings = {
-          'basetime': 250,
-          'startchars': '{\\(',
-          'endchars': '\\)}',
-          'splitchars': '\\|'
-        };
+    init: function( options ) {
+      var settings = {
+        'basetime': 500,
+        'startchars': '{\\(',
+        'endchars': '\\)}',
+        'splitchars': '\\|'
+      };
+      
+      if(options) {
+        $.extend(settings, options);
       }
+
+      return this.each(function(){
+        var $this = $(this),
+            data = $this.data('mstrnscrb');
+        
+        // set the initial data
+        if(!data) {
+          $(this).data('mstrnscrb', {
+            target: $this,
+            transformers: {},
+            settings: settings
+          });
+        }
+      });
     },
     
     // the parse method builds our spans
     parse: function(options) {
-      methods.init();
-      
-      // THINK: this settings thing is pretty silly, but it keeps things consistent across methods. find a better way.
-      var settings = MSTRNSCRB.settings;
-      if(options) {
-        $.extend(settings, options);
-      }
-    
-      this.each(function() {
-        var html = $(this).html();
-        var newhtml = '';
-        var chunks;
-        var counter = 0;
-        var lastIndex = 0;
-      
+      return this.each(function() {
+        var $this = $(this),
+            data = $this.data('mstrnscrb'),
+            settings = data.settings,
+            html = $(this).html(),
+            newhtml = '',
+            chunks = [],
+            lastIndex = 0;
+
         regex = new RegExp('([\\s\\S]*?)(' + settings.startchars + '[\\s\\S]+?' + settings.endchars + ')', 'g'); 
         RegExp.lastIndex = 0;
-      
+
         // find each parsable chunk
         while((chunks = regex.exec(html)) != null) {
           newhtml += chunks[1]; // put the regular stuff back in place
           var segments = chunks[2].substring(2,chunks[2].length - 2).split(new RegExp(settings.splitchars)); // split fancy stuff on pipes
           var items = [];
           var transformer = {};
-          
+
           // if there's an empty segment after the first one, and more segments after that, that first one is a preamble
           // ex. {(name:neato,linear,once||so|soo|sooo|soooo)}
           if(segments.length > 2 && segments[0] && !segments[1]) {
@@ -65,7 +76,7 @@
             } else {
               items[index] = {value: segments[index]};
             }
-            
+
             // allow each extension in turn to parse each segment
             for(var i = MSTRNSCRB.extensions.length - 1; i >= 0; i--) {
               items[index] = MSTRNSCRB.extensions[i].parser(items[index], transformer);
@@ -73,63 +84,67 @@
           };
 
           // set up the span
-          var id = 'mstrnscrb-' + ++counter;
+          var id = 'mstrnscrb-' + ++MSTRNSCRB.counter;
           var span = '<span class="mstrnscrb" id="' + id + '">' + items[0].value + '</span>'; 
           newhtml += span;
 
           transformer.items = items;
-          MSTRNSCRB.transformers[id] = transformer;
+          data.transformers[id] = transformer;
           lastIndex = regex.lastIndex;
         }
 
         newhtml += html.substring(lastIndex); // tack on the end
         $(this).html(newhtml);
       });
-
-      // start the callback if needed
-      // THINK: it might be better to do this once for each element, so we can shut them off individually...
-      if(!MSTRNSCRB.setIntervalId) {
-        methods.start(options);
-      }
-
-      return this;
     },
-    
+
     // the start method sets the timer
     start: function(options) {
-      var settings = MSTRNSCRB.settings;
-      if(options) {
-        $.extend(settings, options);
-      }
+      return this.each(function() {
+        var $this = $(this),
+            data = $this.data('mstrnscrb');
       
-      MSTRNSCRB.setIntervalId = setInterval(function() {
-        $.each(MSTRNSCRB.transformers, function(index, transformer) {          
-          var pickitem;
-          for(var i = MSTRNSCRB.extensions.length - 1; i >= 0; i--) {
-            if(pickitem = MSTRNSCRB.extensions[i].picker(transformer.items, transformer)) {i = -1;} // stop at the first match
-          };
-          
-          MSTRNSCRB.transformers[index].current = pickitem;
-          $('#' + index).html(pickitem.value);
-        });
-      }, settings.basetime);
+        if(data.setIntervalId) {return false;}
+      
+        var settings = data.settings;
+        if(options) {
+          $.extend(settings, options);
+        }
+
+        data.setIntervalId = setInterval(function() {
+          $.each(data.transformers, function(index, transformer) {          
+            var pickitem;
+            for(var i = MSTRNSCRB.extensions.length - 1; i >= 0; i--) {
+              if(pickitem = MSTRNSCRB.extensions[i].picker(transformer.items, transformer, data.transformers)) {i = -1;} // stop at the first match
+            };
+
+            data.transformers[index].current = pickitem;
+            $('#' + index).html(pickitem.value);
+          });
+        }, settings.basetime);
+      });
     },
-    
+
     // the stop method freezes time
     stop: function() {
-      clearInterval(MSTRNSCRB.setIntervalId);
+      return this.each(function() {
+        var $this = $(this),
+            data = $this.data('mstrnscrb');
+      
+        clearInterval(data.setIntervalId);
+        delete(data.setIntervalId);
+      });
     },
-    
+
     // extend adds a new preambler/parser/picker to the mistranscriber
     extend: function(object) {
       if(!object || !object.keyword || !object.preambler || !object.parser || !object.picker) {
         $.error('That is not a valid mistranscribe extension');
       }
-      
-      methods.init();
+
       MSTRNSCRB.extensions.push(object);
     },
-    
+
     // remove an extension
     unextend: function(keyword) {
       MSTRNSCRB.extensions = _.reject(MSTRNSCRB.extensions, function(extension){ return extension.keyword == keyword; });
@@ -137,16 +152,24 @@
   }
   
   
-  // main plugin
+  // the plugin's main loop
   $.fn.mistranscribe = function(method) {
-    if (methods[method]) {
+    if ( methods[method] ) {
+      methods.init.apply( this, arguments );
       return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
     } else if ( typeof method === 'object' || ! method ) {
-      return methods.parse.apply( this, arguments );
+      methods.init.apply( this, arguments );
+      methods.parse.apply( this, arguments );
+      return methods.start.apply( this, arguments );
     } else {
       $.error( 'Method ' +  method + ' does not exist on jQuery.mistranscribe' );
     }    
   };
+  
+  
+  
+  // Add some extra extensions for flavor and awesome
+  // A note about extensions: they run in reverse of the order they are added, which can cause some spectacular conflicts. Be wary of anyone hawking magical extension-conflict-resolving extensions: they never actually work, and typically just end up conflicting with each other. 
   
   
   // add the basic functionality
@@ -162,10 +185,6 @@
       return items[Math.round(Math.random() * (items.length - 1))]; // pick a random item
     }
   });
-  
-  
-  // Add some extra extensions for flavor and awesome
-  // A note about extensions: they run in reverse of the order they are added, which can cause some spectacular conflicts. Be wary of anyone hawking magical extension-conflict-resolving extensions: they never actually work, and typically just end up conflicting with each other. 
   
   
   // show segments in varying ratios
@@ -314,11 +333,11 @@
     parser: function(item, transformer) {
       return item;
     },
-    picker: function(items, transformer) {
+    picker: function(items, transformer, transformers) {
       var return_item;
       
       if(transformer.copy) {
-        $.each(MSTRNSCRB.transformers, function(index, test_trans) {          
+        $.each(transformers, function(index, test_trans) {          
           if(test_trans.name == transformer.copy) {
             return_item = test_trans.current;
             return false;
@@ -327,7 +346,7 @@
       }
       
       if(transformer.follow) {
-        $.each(MSTRNSCRB.transformers, function(index, test_trans) {          
+        $.each(transformers, function(index, test_trans) {          
           if(test_trans.name == transformer.follow) {
             for (var j = test_trans.items.length - 1; j >= 0; j--){
               if(test_trans.items[j].value == test_trans.current.value) {
@@ -425,6 +444,6 @@
     };
   }
   
-  // TODOs: degrade nicely, non-globalize, different setInterval loops etc for different invocations, more extensions, README, updatable textarea, examples, base 
+  // TODOs: degrade nicely, README, examples, base preamble extension 
   
 })(jQuery);
